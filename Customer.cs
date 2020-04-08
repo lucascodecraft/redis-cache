a50f6670ed26e936366f12b8ecaebdab4bda1aa1
lucaslima10077 <lucaslima10077@gmail.com>
2020-04-08 19:41:54 -0300

    delete past

---
+++ Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedisCache
+{
+    public class Customer
+    {
+        public Customer(string cpf)
+        {
+            Cpf = cpf;
+        }
+
+        public string Cpf { get; private set; }
+        public string Name { get; set; }
+        public string Document { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ServiceStack.Redis;
+
+namespace RedisCache
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = "localhost:6379";
+            var cpf = "123.456.789.12";
+
+            var customer = new Customer(cpf) { Document = "15114818", Name = "Fulano" };
+        
+            using (var redisCustumer = new RedisClient(host))
+            {
+                redisCustumer.Set<Customer>(cpf, customer, new TimeSpan(0, 0, 50));
+
+                var customerInf = redisCustumer.Get<Customer>(cpf);
+            }
+        }
+    }
+}
